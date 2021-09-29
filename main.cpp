852fe514e6986773425786ee48dfe6e3753a8662
root <norman@chapman.edu>
2021-09-29 02:47:23 +0000

    made some changes to main to check for user error

--- main.cpp
+++ main.cpp
@@ -7,12 +7,11 @@ Assignment 2
 */
 
 #include "fileprocessor.h"
-//#include "translator.h"
 
 int main(int argc, char** argv){ // main method
     FileProcessor *fp = new FileProcessor(); // creates file processor object to be able to use methods from file processor class
 
-    if(argc < 3) { //checks to see if correct amount of arguments were provided in command line
+    if(argc < 4) { //checks to see if correct amount of arguments were provided in command line
         cout << "whoops! sorry! There was an incorrect amount of arguments provided." << endl;
         cout << "Please run like: \"./assignment2.out englishfile.txt tutnesefile.txt\"" << endl;
         cout << "                               or                                     " << endl;
@@ -26,15 +25,16 @@ int main(int argc, char** argv){ // main method
     string input = argv[1]; // takes the 2nd argument command line which is the input file 
     string output = argv[2]; // takes the 3rd argument command line which is the output file
     string translationPath = "";
-    if(argc == 4){ //check to see if there is an argument for translationPath
-        translationPath = argv[3]; // takes the 4th argument in command line which is which way to translate, E2T is English to Tutnese and T2E is Tutnese to English
-    }
-    if(input.substr(input.length() - 4) != ".txt"){
+    translationPath = argv[3]; // takes the 4th argument in command line which is which way to translate, E2T is English to Tutnese and T2E is Tutnese to English
+    
+    if(input.substr(input.length() - 4) != ".txt"){ //check if file has .txt extension
         cout << "please input a .txt file for the input" << endl;
 
         return -1;
-    }else if (output.length() < 4){
+    }else if (output.length() < 4){ //check if file has a .txt extension 
         cout << "please input a .txt file for the output" << endl;
+
+        return -1;
     }
 
     if(output.substr(output.length() - 4) != ".txt") {
@@ -43,14 +43,12 @@ int main(int argc, char** argv){ // main method
         return -1;
     } else if (output.length() < 4){
         cout << "please input a .txt file for the output" << endl;
-    }
-    
-    if (translationPath == ""){ //defaults to english to tutnese if no translation path is given
-        fp->processFile(input, output); // calls processFile method with command line arguments as arguments for method, if no third argument is provided translates from english to tutnese
 
-    } else {
-        fp->processFile(input, output, translationPath); //if no translation path is given then defaults to english to tutnese
+        return -1;
     }
+
+    fp->processFile(input, output, translationPath); //translates file depending on translation path from english to tutnese or tutnese to english
+    
     
 
     delete fp; //deletes file processor object now that we are done using it
